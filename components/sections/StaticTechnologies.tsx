3e900134c64485a5242c79035293ed719934b81b
Luiz Mesquita <luizalbertobm@gmail.com>
2025-06-30 23:51:49 +0100

    refactor: atualizar cores dos ícones para 'text-white' no componente StaticTechnologies e otimizar a tradução no StaticTestimonials

--- components/sections/StaticTechnologies.tsx
+++ components/sections/StaticTechnologies.tsx
@@ -61,22 +61,22 @@ export default function StaticTechnologies({ translations }: StaticTechnologiesP
             data-aos-offset="100"
             data-aos-delay="200"
           >
-            <FaJs className="w-11 h-11 text-yellow-400 hover:scale-110 transition-transform" />
-            <FaPhp className="w-11 h-11 text-indigo-300 hover:scale-110 transition-transform" />
-            <FaReact className="w-11 h-11 text-cyan-400 hover:scale-110 transition-transform" />
-            <FaNodeJs className="w-11 h-11 text-green-400 hover:scale-110 transition-transform" />
-            <FaDocker className="w-11 h-11 text-blue-400 hover:scale-110 transition-transform" />
-            <FaBootstrap className="w-11 h-11 text-purple-400 hover:scale-110 transition-transform" />
-            <FaSymfony className="w-11 h-11 text-beePrimary-lightest hover:scale-110 transition-transform" />
-            <FaLaravel className="w-11 h-11 text-red-400 hover:scale-110 transition-transform" />
-            <FaAws className="w-11 h-11 text-orange-400 hover:scale-110 transition-transform" />
-            <FaAndroid className="w-11 h-11 text-green-500 hover:scale-110 transition-transform" />
-            <FaApple className="w-11 h-11 text-gray-300 hover:scale-110 transition-transform" />
+            <FaJs className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaPhp className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaReact className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaNodeJs className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaDocker className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaBootstrap className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaSymfony className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaLaravel className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaAws className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaAndroid className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaApple className="w-11 h-11 text-white hover:scale-110 transition-transform" />
             <FaGithub className="w-11 h-11 text-white hover:scale-110 transition-transform" />
-            <FaLinux className="w-11 h-11 text-yellow-400 hover:scale-110 transition-transform" />
-            <FaDigitalOcean className="w-11 h-11 text-blue-500 hover:scale-110 transition-transform" />
-            <IconRuby className="w-11 h-11 text-red-600 hover:scale-110 transition-transform" />
-            <IconBxlFlutter className="w-11 h-11 text-blue-400 hover:scale-110 transition-transform" />
+            <FaLinux className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <FaDigitalOcean className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <IconRuby className="w-11 h-11 text-white hover:scale-110 transition-transform" />
+            <IconBxlFlutter className="w-11 h-11 text-white hover:scale-110 transition-transform" />
           </div>
         </div>
       </div>
